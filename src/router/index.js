453a17285189e39cbf6691662e5f2ba1577b5040
yaojinwu <350072095@qq.com>
2017-05-02 18:03:40 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,28 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Vue from 'vue';
+import Router from 'vue-router';
+// import Hello from '@/components/Hello';
 
-Vue.use(Router)
-
+Vue.use(Router);
+const Goods = {template: '<div>goods</div>'};
+const Ratings = {template: '<div>ratings</div>'};
+const Seller = {template: '<div>seller</div>'};
+const routes = [
+  {
+    path: '/goods',
+    // name: 'Hello',
+    component: Goods
+  },
+  {
+    path: '/ratings',
+    // name: 'Hello',
+    component: Ratings
+  },
+  {
+    path: '/seller',
+    // name: 'Hello',
+    component: Seller
+  }
+];
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
-})
+  routes
+});
